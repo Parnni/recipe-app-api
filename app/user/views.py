0d75c0a4e4690ca6c77e848511aba3c12efad0a3
Parnni <prajapaticamps8976@gmail.com>
2023-01-28 22:34:12 +0530

    Add token authentication

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,15 @@
 """API for creating a user."""
 
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.generics import CreateAPIView
-from user.serializers import UserSerializer
+from rest_framework.settings import api_settings
+from user.serializers import AuthTokenSerializer, UserSerializer
 
 
-class CreateUserAPI(CreateAPIView):
+class CreateUserView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
