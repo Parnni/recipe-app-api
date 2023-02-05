3bb9b72e77a417572e9f9a1110a8ae9b553b8aca
Parnni <prajapaticamps8976@gmail.com>
2023-02-05 23:33:02 +0530

    Fix the incorrect fields.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,5 +5,12 @@ from rest_framework import serializers
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
-        fields = ["id", "user", "title", "description", "time", "price", "link"]
+        fields = ["id", "title", "time", "price"]
         read_only_fields = ["id"]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """More detailed serializer."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description", "link"]
